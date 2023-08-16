1e34bab3270825573ccea974922b406bda606bde
Roxana <ms.roxanarahimi@gmail.com>
2023-08-16 22:38:19 +0330

    search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\BlogResource;
 use App\Http\Resources\ProductResource;
 use App\Models\Article;
+use App\Models\Blog;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Lcobucci\JWT\Exception;
@@ -14,13 +16,19 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         try {
-            if ($request['term'] != ''){
-                $data = Product::where('title',  'Like', '%' . $request['term'] . '%')->where('active',1)->get();
-                return response(ProductResource::collection($data), 200);
-            }else{
+            if ($request['term'] != '') {
+                $products = Product::where('title', 'Like', '%' . $request['term'] . '%')->where('active', 1)->get();
+                $articles = Article::where('title', 'Like', '%' . $request['term'] . '%')->where('active', 1)->get();
+                $blogs = Blog::where('title', 'Like', '%' . $request['term'] . '%')->where('active', 1)->get();
+                return response([
+                    "products" => ProductResource::collection($products),
+                    "articles" => ArticleResource::collection($articles),
+                    "blogs" => BlogResource::collection($blogs)
+                ], 200);
+            } else {
                 return response([], 200);
             }
-        }catch (Exception $exception){
+        } catch (Exception $exception) {
             return response($exception);
         }
     }
